909ccc892cc9f1f132fdb8bb3bf81ee58237b494
Konrad Drozd <konrad.drozd@tieto.com>
2020-01-08 18:10:05 +0100

    Add small data set

--- reset.sh
+++ reset.sh
@@ -7,3 +7,7 @@ hadoop fs -rm -R -f -skipTrash rezultaty
 hive -e "drop database if exists imdb CASCADE;"
 hive -e "drop database if exists etap1 CASCADE;"
 hive -e "drop database if exists etap2 CASCADE;"
+
+hive -e "drop database if exists func_etap2 CASCADE;"
+hive -e "DROP DATABASE IF EXISTS func_etap1 CASCADE;"
+hive -e "DROP DATABASE IF EXISTS func_imdb CASCADE;"
