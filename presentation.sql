0a67e74d8d5a973e9cc353b213d626c96119cd9b
Michał Legut <michallegut@wp.pl>
2020-01-08 16:54:38 +0100

    Update presentation.sql

--- presentation.sql
+++ presentation.sql
@@ -2,6 +2,6 @@ CREATE TABLE actor_popularity (nconst VARCHAR(255) PRIMARY KEY, year INT, popula
 CREATE TABLE director_popularity (nconst VARCHAR(255) PRIMARY KEY, year INT, popularity FLOAT, primaryname VARCHAR(255));
 CREATE TABLE series_popularity (tconst VARCHAR(255) PRIMARY KEY, popularity FLOAT, primarytitle VARCHAR(255), seasonnumber INT);
 
-LOAD DATA INFILE './actor_popularity.csv' INTO TABLE actor_popularity
-LOAD DATA INFILE './director_popularity.csv' INTO TABLE director_popularity
-LOAD DATA INFILE './series_popularity.csv' INTO TABLE series_popularity
+LOAD DATA INFILE './przetwarzanie/actor_popularity.csv' INTO TABLE actor_popularity
+LOAD DATA INFILE './przetwarzanie/director_popularity.csv' INTO TABLE director_popularity
+LOAD DATA INFILE './przetwarzanie/series_popularity.csv' INTO TABLE series_popularity
