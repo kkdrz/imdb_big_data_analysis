738a4414c57dcbb40f67a28ee854dff103f63e6a
Michał Legut <michallegut@wp.pl>
2020-01-08 17:04:38 +0100

    Update exportToCSV.sh

--- przetwarzanie/exportToCSV.sh
+++ przetwarzanie/exportToCSV.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-hive -e "SELECT * FROM etap2.series_popularity" | sed \$d | sed \$d > ./series_popularity.csv
-hive -e "SELECT * FROM etap2.director_popularity" | sed \$d | sed \$d > ./director_popularity.csv
-hive -e "SELECT * FROM etap2.actor_popularity" | sed \$d | sed \$d > ./actor_popularity.csv
+hive -e "SELECT * FROM etap2.series_popularity" | sed \$d | sed \$d > ./series_popularity.tsv
+hive -e "SELECT * FROM etap2.director_popularity" | sed \$d | sed \$d > ./director_popularity.tsv
+hive -e "SELECT * FROM etap2.actor_popularity" | sed \$d | sed \$d > ./actor_popularity.tsv
