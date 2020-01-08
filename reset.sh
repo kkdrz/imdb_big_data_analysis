c3ce1e11bcd8ce53eefc06cb85f115fcb2e17f04
Konrad Drozd <konrad.drozd@tieto.com>
2020-01-08 16:56:05 +0100

    Modify reset.sh to remove temp databases

--- reset.sh
+++ reset.sh
@@ -5,3 +5,5 @@ hadoop fs -rm -R -f -skipTrash posrednie
 hadoop fs -rm -R -f -skipTrash rezultaty
 
 hive -e "drop database if exists imdb CASCADE;"
+hive -e "drop database if exists etap1 CASCADE;"
+hive -e "drop database if exists etap2 CASCADE;"
