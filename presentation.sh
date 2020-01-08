4ec423f7a7ad31dab10cd87e66c001a2b2a4e450
Michał Legut <michallegut@wp.pl>
2020-01-08 16:52:26 +0100

    Update presentation.sh

--- presentation.sh
+++ presentation.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-hive -e "SELECT * FROM etap2.series_popularity" | sed \$d | sed \$d > ./przetwarzanie/series_popularity.csv
-hive -e "SELECT * FROM etap2.director_popularity" | sed \$d | sed \$d > ./przetwarzanie/director_popularity.csv
-hive -e "SELECT * FROM etap2.actor_popularity" | sed \$d | sed \$d > ./przetwarzanie/actor_popularity.csv
 mysql -uroot -pcloudera -e 'CREATE DATABASE presentation CHARACTER SET UTF8'
 mysql -uroot -pcloudera presentation < ./presentation.sql
