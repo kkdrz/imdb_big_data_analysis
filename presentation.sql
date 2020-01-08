71fdc17ac4a7019b7f1e8aa021ac3e5bd4a8c949
Michał Legut <michallegut@wp.pl>
2020-01-08 17:03:44 +0100

    Update presentation.sql

--- presentation.sql
+++ presentation.sql
@@ -1,7 +1,7 @@
 CREATE TABLE actor_popularity (nconst VARCHAR(255) PRIMARY KEY, year INT, popularity FLOAT, primaryname VARCHAR(255));
 CREATE TABLE director_popularity (nconst VARCHAR(255) PRIMARY KEY, year INT, popularity FLOAT, primaryname VARCHAR(255));
 CREATE TABLE series_popularity (tconst VARCHAR(255) PRIMARY KEY, popularity FLOAT, primarytitle VARCHAR(255), seasonnumber INT);
-
-LOAD DATA INFILE './przetwarzanie/actor_popularity.csv' INTO TABLE actor_popularity
-LOAD DATA INFILE './przetwarzanie/director_popularity.csv' INTO TABLE director_popularity
-LOAD DATA INFILE './przetwarzanie/series_popularity.csv' INTO TABLE series_popularity
+                                                                                                                   
+LOAD DATA LOCAL INFILE './przetwarzanie/actor_popularity.tsv' INTO TABLE actor_popularity FIELDS TERMINATED BY '\t';
+LOAD DATA LOCAL INFILE './przetwarzanie/director_popularity.tsv' INTO TABLE director_popularity FIELDS TERMINATED BY '\t';
+LOAD DATA LOCAL INFILE './przetwarzanie/series_popularity.tsv' INTO TABLE series_popularity FIELDS TERMINATED BY '\t';
